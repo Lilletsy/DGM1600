4992d5c06380ca2c2e8c151cba84b7f28dd2e044
Rose <10617024@my.uvu.edu>
2016-12-05 20:34:17 -0700

    commiting final game

---
+++ scriptingclassgame/Assets/Destroy_crates.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy_crates : MonoBehaviour {
+    public GameObject debris;
+
+
+void OnCollisionEnter(Collision collision){
+    if(collision.gameObject.tag == "Projectile"){
+    Instantiate(debris,transform.position,transform.rotation);
+    Destroy (gameObject);
+}
+}
+
+// Update is called once per frame
+void Update () {
+//Destroy (gameObject, countDown);
+}
+}
